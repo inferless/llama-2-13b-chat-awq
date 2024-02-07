255dea05cc89eb8081c5e8d9da7826eabf6a9a18
rbgo404 <150957746+rbgo404@users.noreply.github.com>
2024-02-07 11:12:26 +0530

    Create input_schema.py

---
+++ input_schema.py
@@ -0,0 +1,8 @@
+INPUT_SCHEMA = {
+    "message": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["How to count all the stars in the night sky?"]
+    }
+}
